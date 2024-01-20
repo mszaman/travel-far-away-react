d9c5753672d4a7d1901663f006ad5f0ff1b309e7
mszaman <munshisayeduzzaman@gmail.com>
2024-01-21 00:35:34 +0600

    fixed some issues.

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,18 @@ import Form from "./components/Form";
 import Logo from "./components/Logo";
 import PackingList from "./components/PackingList";
 import Stats from "./components/Stats";
-import { itemList } from "./data";
 
 function App() {
   const [items, setItems] = useState([]);
-  // console.log(items);
 
   const handleAddItems = (item) => {
     setItems((items) => [...items, item]);
   };
 
   const handleToggleItem = (id) => {
-    console.log(id);
     setItems((items) =>
       items.map((i) => (i.id === id ? { ...i, packed: !i.packed } : i)),
     );
-    console.log(items);
   };
 
   const handleDeleteItem = (id) => {
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,20 +3,14 @@ export default function Stats({ items }) {
   const packedItems = items.filter((item) => item.packed).length;
   const percentage = Math.round((packedItems / totalItems) * 100);
 
-  if (percentage === 100) {
-    return (
-      <footer className="bg-aqua py-10 text-2xl font-bold">
-        <em>You packed all items. Now you are ready to go ✈️</em>
-      </footer>
-    );
-  }
-
   return (
     <footer className="bg-aqua py-10 text-2xl font-bold">
       <em>
-        {totalItems > 0
-          ? `💼 You have ${totalItems} items on your list, and you already packed ${packedItems}(${percentage}%)`
-          : `Start adding some items to your packing list 🚀`}
+        {percentage === 100
+          ? `You packed all items. Now you are ready to go ✈️`
+          : totalItems > 0
+            ? `💼 You have ${totalItems} items on your list, and you already packed ${packedItems}(${percentage}%)`
+            : `Start adding some items to your packing list 🚀`}
       </em>
     </footer>
   );
